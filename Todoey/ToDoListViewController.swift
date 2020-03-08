ffc5ff83178d391801143b05cb0003b7d8cacc71
yunotran9b <yunotran123@gmail.com>
2020-03-08 10:22:13 +0700

    Updated Delete func

--- Todoey/ToDoListViewController.swift
+++ Todoey/ToDoListViewController.swift
@@ -14,7 +14,9 @@ class ToDoListViewController: UITableViewController {
   
   override func viewDidLoad() {
     super.viewDidLoad()
-    // Do any additional setup after loading the view.
+    let leftButton = UIBarButtonItem(title: "Edit", style: UIBarButtonItem.Style.plain, target: self, action: Selector("showEditing:"))
+    self.navigationItem.leftBarButtonItem = leftButton
+    leftButton.tintColor = .white
   }
   
   override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -23,8 +25,81 @@ class ToDoListViewController: UITableViewController {
   
   override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
     let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
-     = toDoArray[indexPath.row]
+    cell.textLabel?.text = toDoArray[indexPath.row]
+    return cell
   }
+  
+  override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+    //print(toDoArray[indexPath.row])
+    
+    if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+      tableView.cellForRow(at: indexPath)?.accessoryType = .none
+    } else {
+      tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+    }
+    
+    tableView.deselectRow(at: indexPath, animated: true)
+  }
+  @IBAction func addToDo(_ sender: UIBarButtonItem?) {
+    
+    var textField = UITextField()
+    
+    let alert = UIAlertController(title: "Add New Item",
+                                  message: "",
+                                  preferredStyle: .alert)
+    let action = UIAlertAction(title: "Add Item", style: .default) { (action) in
+      if let text = textField.text {
+        if text == "" {
+          self.showAlert(message: "There nothing to add")
+        } else {
+          self.toDoArray.append(text)
+        }
+      } else {
+        self.showAlert(message: "Add item failed, please try again")
+      }
+      self.tableView.reloadData()
+    }
+    
+    alert.addTextField { (alertTextField) in
+      alertTextField.placeholder = "Enter your item"
+      textField = alertTextField
+    }
+    alert.addAction(action)
+    
+    present(alert, animated: true, completion: nil)
+  }
+  
+  func showAlert(message: String) {
+    let alertController = UIAlertController(title: "Error", message:
+      message, preferredStyle: .alert)
+    alertController.addAction(UIAlertAction(title: "Dismiss", style: .default))
+    
+    self.present(alertController, animated: true, completion: nil)
+  }
+  
+  override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+    if editingStyle == .delete {
+      toDoArray.remove(at: indexPath.row)
+      tableView.deleteRows(at: [indexPath], with: .fade)
+    } else if editingStyle == .insert {
+      addToDo(nil)
+    }
+  }
+  
+  @objc func showEditing(_ sender: UIBarButtonItem)
+  {
+    if(self.tableView.isEditing == true)
+    {
+      self.tableView.isEditing = false
+      self.navigationItem.rightBarButtonItem?.title = "Done"
+    }
+    else
+    {
+      self.tableView.isEditing = true
+      self.navigationItem.rightBarButtonItem?.title = "Edit"
+    }
+  }
+  
   
 }
 
